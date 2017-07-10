6e7ae4b55c4249333ceaaea86ac692e2714ca579
ueg1990 <uehtesham90@gmail.com>
2017-07-09 22:35:07 -0400

    Implement ability to generate fake data with a nested schema
    My first ever recursive method!!! :D More specifically, first time where I saw a
    need for a recursive solution and implemented one. And the funny part is that
    I still dont understand it completely lol!

--- faker_schema/faker_schema.py
+++ faker_schema/faker_schema.py
@@ -18,27 +18,29 @@ class FakerSchema(object):
         self._schema = schema
         
     def generate_fake(self, iterations=1):
-        result = [self._generate_one_fake() for _ in range(iterations)]
+        result = [self._generate_one_fake(self._schema) for _ in range(iterations)]
         return result[0] if len(result) == 1 else result
 
-    def _generate_one_fake(self):
-        fake_data = {}
-        for key, key_type in self._schema.items():
-            try:
-                fake_data[key] = self._generate(key_type)
-            except AttributeError:
-                raise ValueError('No faker found for key "{}", which has type "{}".'
-                                 .format(key, key_type))
-        return fake_data
+    def _generate_one_fake(self, schema):
+        """
+        Recursively traverse schema dictionary and for each "leaf node", evaluate the fake
+        value
 
-    def _generate(self, key_type):
-        if isinstance(key_type, list):
-            result = []
-            for item in key_type:
-                for k, v in list(item.items()):
-                    result.append({k: getattr(self._faker, v)()})
-            return result
-        return getattr(self._faker, key_type)()
+        Implementation:
+        For each key-value pair:
+        1) If value is not an iterable (i.e. dict or list), evaluate the fake data (base case)
+        2) If value is a dictionary, recurse
+        3) If value is a list, iteratively recurse over each item
+        """
+        data = {}
+        for k, v in schema.items():
+            if isinstance(v, dict):
+                data[k] = self._generate_one_fake(v)
+            elif isinstance(v, list):
+                data[k] = [self._generate_one_fake(item) for item in v]
+            else:
+                data[k] = getattr(self._faker, v)()
+        return data
 
     @classmethod
     def from_json_file(cls, json_file, *args, **kwargs):
